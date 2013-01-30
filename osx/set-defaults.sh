5768cd751ea2f055bf06fec439917f77ae66a41d
Jeremy Herrman <jeremy@jherrman.com>
2013-01-30 15:37:54 -0500

    Classing it up.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -2,8 +2,6 @@
 
 # Sets reasonable OS X defaults.
 #
-# Or, in other words, set shit how I like in OS X.
-#
 # Copied from: https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 #
 # Run ./set-defaults.sh and you'll be good to go.
